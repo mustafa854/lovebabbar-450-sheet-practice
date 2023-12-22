92152adc39375587eeb7103a87b151552fe71076
Mustafa <mustafabidiwala84@gmail.com>
2023-12-22 12:47:49 +0530

    String Q3 Broiler Plate Added

---
+++ StringQ3.py
@@ -0,0 +1,17 @@
+class DuplicateCharacters:
+    def findDuplicateCharacters(self, s: str) -> None:
+        # Your code to find and print duplicate characters with their occurrences goes here
+        pass
+
+
+# Driver code to test the findDuplicateCharacters method
+if __name__ == "__main__":
+    # Test case
+    input_string = "geeksforgeeks"
+    
+    # Create an instance of the DuplicateCharacters class
+    solution = DuplicateCharacters()
+    
+    print("Input:", input_string)
+    print("Output:")
+    solution.findDuplicateCharacters(input_string)
